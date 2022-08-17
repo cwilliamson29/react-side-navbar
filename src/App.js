fd62ab7660f6fe23f010e3d6dd7b9cc281066510
Chris Williamson <chriswill0629@gmail.com>
2022-08-17 11:40:24 -0400

    Setting initial bootstrap sizing and bounds

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ function App() {
   return (
     <div className="container-fluid bg-secondary flex-start">
       <div className="row">
-        <div className="col-md-2 bg-primary">
+        <div className="col-12 bg-primary d-block d-sm-block d-md-none d-lg-none">
+          <div className=" bg-primary">asdfasdfasdf</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-5 col-lg-3 col-xlg-2 bg-primary d-none d-sm-none d-md-block">
           <div className="box-inner bg-primary">asdf</div>
         </div>
-        <div className="col-md-7 bg-warning">asdf</div>
+        <div className="col-12 col-md-7 col-lg-9 col-xlg-10 bg-warning">
+          asdf
+        </div>
       </div>
     </div>
   );
